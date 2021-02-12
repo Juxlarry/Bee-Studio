1b357b9fabe11dd746da82bd97cfacddbf0f9253
Lawrence D <60719231+Juxlarry@users.noreply.github.com>
2021-02-12 14:19:02 -0800

    Online Email Service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :port                 => "587",
     :domain               => "localhost:3000",
     :user_name            => "juxlarry@gmail.com",
-    :password             => "<%= ENV['EMAIL_PASSWORD'] %>",
+    :password             => "<%= ENV['GMAIL_PASSWORD'] %>",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
